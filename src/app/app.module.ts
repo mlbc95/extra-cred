d009d6b20ab2823e8457cca2f18b511c46bbb8b5
Langi <mlnp3@umsl.edu>
2017-11-08 18:54:52 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
+
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DevExtremeModule} from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../app/components/login/login.component';
+import { RegisterComponent } from '../app/components/register/register.component';
+import { HomeComponent } from '../app/components/home/home.component';
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
+import { SearchComponent } from '../app/components/search/search.component';
+import { ClassesComponent } from '../app/components/classes/classes.component';
+import { PublicComponent } from '../app/components/layouts/public/public.component';
+import { UserComponent } from '../app/components/layouts/user/user.component';
+import { NavbarComponent } from '../app/components/navbar/navbar.component';
+import { SidebarComponent } from '../app/components/sidebar/sidebar.component';
+
+const appRoutes: Routes= [
+  {path:'', component:PublicComponent, children:[
+    {path:'', component:HomeComponent},
+    {path:'login', component:LoginComponent},
+    {path:'register',component:RegisterComponent}
+  ]},
+  {path:'user',component:UserComponent, children:[
+    {path:'dashboard',component:DashboardComponent}
+    
+  ]}
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    SearchComponent,
+    ClassesComponent,
+    PublicComponent,
+    UserComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DevExtremeModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
